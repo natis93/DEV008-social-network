86f02c01574faf2892796c9e78e4733795e9dcfd
natis93 <natis93_2011@hotmail.com>
2023-07-03 10:05:36 -0500

    configuracion firebase

---
+++ src/components/register.js
@@ -0,0 +1,20 @@
+export const Register = (onNavigate) => {
+  const homeDiv = document.createElement('div');
+  homeDiv.textContent = 'Start your adventure here';
+  const buttonHome = document.createElement('button');
+  buttonHome.textContent = 'Home';
+  const inputUser = document.createElement('input');
+  const inputEmail = document.createElement('input');
+  const inputPassword = document.createElement('input');
+  const buttonDataRegister = document.createElement('button');
+  buttonDataRegister.textContent = 'Submit register';
+
+  homeDiv.appendChild(inputUser);
+  homeDiv.appendChild(inputEmail);
+  homeDiv.appendChild(inputPassword);
+  homeDiv.appendChild(buttonDataRegister);
+
+  buttonHome.addEventListener('click', () => onNavigate('/'));
+  homeDiv.appendChild(buttonHome);
+  return homeDiv;
+};
