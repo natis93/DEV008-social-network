12f6ff77d28808996d309e3a4432d219a3a84bb9
natis93 <natis93_2011@hotmail.com>
2023-07-04 16:09:44 -0500

    cambio1

--- src/components/login.js
+++ src/components/login.js
@@ -1,24 +1,30 @@
-export const Login = (onNavigate) => {
-    const homeDiv = document.createElement('div');
-    homeDiv.textContent = 'Welcome back to the orbit!';
-    const buttonHome = document.createElement('button');
+
+
+export const Login = (onNavigate) => { // Define una función llamada "login" que recibe un parámetro "onNavigate"
+    const homeDiv = document.createElement('div');// Crear un div principal
+    homeDiv.textContent = 'Welcome back to the orbit!';// Establecer el texto del div principal
+
+    const buttonHome = document.createElement('button');// Crear un botón para ir atrás
     buttonHome.textContent = 'Go back';
 
-const inputMailL= document.createElement('input');
+const inputMailL= document.createElement('input');// Crear inputs para el correo electrónico y la contraseña
 const inputPassL= document.createElement('input');
-const buttonLoginL= document.createElement('button');
-buttonLoginL.textContent= 'Login';
-const buttonLoginG= document.createElement('button');
-buttonLoginG.textContent= 'Login with google';
 
+const buttonLoginL= document.createElement('button'); // Crear un botón para iniciar sesión
+buttonLoginL.textContent= 'Login';
+
+const buttonLoginG= document.createElement('button');// Crear un botón para iniciar sesión con Google
+buttonLoginG.textContent= 'Login with google';
+ // Agregar los inputs y los botones al div principal
 homeDiv.appendChild(inputMailL);
 homeDiv.appendChild(inputPassL);
 homeDiv.appendChild(buttonLoginL);
 homeDiv.appendChild(buttonLoginG);
 
 
-
+ // Agregar un evento al botón de ir atrás para llamar a la función "onNavigate" cuando se haga clic
     buttonHome.addEventListener('click', () => onNavigate('/'));
     homeDiv.appendChild(buttonHome);
+    // Retornar el div principal
     return homeDiv;
   };
