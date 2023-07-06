41566f46f569f473b3e2f92cfa3080d61b195641
CarolynYQ <carolynyaker@gmail.com>
2023-07-06 14:28:09 -0500

    Estilos register

--- src/components/register.js
+++ src/components/register.js
@@ -1,10 +1,29 @@
-import { createUser } from "../lib/firebase";
+import { createUser, addUser } from "../lib/auth";
 
 export const register = (onNavigate) => {
     const homeDiv = document.createElement("div");
-    homeDiv.textContent = "Start your adventure here";
+    homeDiv.className = "container";
 
+    const headerDiv = document.createElement("div");
+    headerDiv.className = "container__header";
+    homeDiv.appendChild(headerDiv);
+
+    const iconDiv = document.createElement("div");
+    iconDiv.className = "container__icon";
+    headerDiv.appendChild(iconDiv);
+
+    const icon = document.createElement("img");
+    icon.className = "icon";
+    icon.src = "../Images/icon.png";
+    iconDiv.appendChild(icon);
+    
+    const titleDiv = document.createElement("div");
+    titleDiv.textContent = "Start your adventure here";
+    titleDiv.className = "container__title";
+    headerDiv.appendChild(titleDiv);
+    
     const buttonHome = document.createElement("button");
+    buttonHome.className = "button button-home";
     buttonHome.textContent = "Home";
 
     const form = document.createElement("form");
@@ -23,15 +42,18 @@ export const register = (onNavigate) => {
 
     const inputPassword = document.createElement("input");
     inputPassword.className = "input input-password";
+    inputPassword.type = "password";
     inputPassword.placeholder = "Password";
     form.appendChild(inputPassword);
     
-    const buttonDataRegister = document.createElement("button");
-    buttonDataRegister.className = "button-data-register";
-    buttonDataRegister.textContent = "Register";
+    const buttonDataRegister = document.createElement("input");
+    buttonDataRegister.className = "button button-register";
+    //buttonDataRegister.textContent = "Register";
     buttonDataRegister.type = "submit";
+    buttonDataRegister.value = "Register"
     form.appendChild(buttonDataRegister);
     homeDiv.appendChild(form);
+
     buttonDataRegister.addEventListener("click", (e) => {
         e.preventDefault();
         const registerUserName = inputUser.value;
@@ -40,7 +62,9 @@ export const register = (onNavigate) => {
         createUser(registerEmail, registerPassword);
     });
     buttonHome.addEventListener("click", () => onNavigate("/"));
-    homeDiv.appendChild(buttonHome);
+    form.appendChild(buttonHome);
+
+    
 
     return homeDiv;
 };
