7011957ddc88d46a26679b715792d73f21a4ee05
CarolynYQ <carolynyaker@gmail.com>
2023-07-18 21:34:48 -0500

    Creación y maquetación de feed

--- src/lib/auth.js
+++ src/lib/auth.js
@@ -2,6 +2,7 @@ import {
   createUserWithEmailAndPassword,
   signInWithEmailAndPassword,
   signInWithRedirect,
+  onAuthStateChanged
 } from 'firebase/auth';
 import { auth, provider } from './firebase';
 
@@ -16,4 +17,15 @@ export const signInUser = (email, password) =>
 export const signInGoogle = () => 
   signInWithRedirect(auth, provider); // Return the result of the function execution
 
-
+export const stateChanged = () =>
+  onAuthStateChanged(auth, (user) => {
+    if (user) {
+      // User is signed in, see docs for a list of available properties
+      // https://firebase.google.com/docs/reference/js/auth.user
+      const uid = user.uid;
+      // ...
+    } else {
+      // User is signed out
+      // ...
+    }
+  });
